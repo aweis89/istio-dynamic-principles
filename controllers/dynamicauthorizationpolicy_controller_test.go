9618081d5e53f03949a3d8001b46a4b68997a8f9
Aaron Weisberg <aaronweisberg@gmail.com>
2022-03-11 23:19:34 -0600

    adds table tests

--- controllers/dynamicauthorizationpolicy_controller_test.go
+++ controllers/dynamicauthorizationpolicy_controller_test.go
@@ -27,150 +27,129 @@ import (
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var (
-	timeout  = time.Second * 10
-	interval = time.Millisecond * 250
-
-	fakeClient = Label("fake")
-	realClient = Label("real")
+	timeout  = time.Second * 5        // nolint:gochecknoglobals
+	interval = time.Millisecond * 250 // nolint:gochecknoglobals
 )
 
-//var _ = Describe("DynamicAuthorizationPolicy controller", func() {
-//	genTests(time.Duration(0), useFakeClient)
-//})
-
-// func genTests(waitBetweenCreate time.Duration, fakeClient bool) {
 var _ = Describe("DynamicAuthorizationPolicy controller", func() {
-	It("When Pod is exists mathching selectors, policies are added to DAP", func() {
-		ctx := context.Background()
+	labelSel := map[string]string{"labelKey": "labelVal"}
 
-		name, namespace := "name", "default"
-		labelSel := map[string]string{"labelKey": "labelVal"}
+	dynamicPolicy := v1.DynamicPolicy{
+		PodSelectors: labelSel,
+		Name:         "policy",
+		TrustDomain:  "cluster.local",
+	}
 
-		dynamicPolicy := v1.DynamicPolicy{
-			PodSelectors: labelSel,
-			Name:         "policy",
-			TrustDomain:  "cluster.local",
-		}
+	namespace := "default"
 
-		dap := &v1.DynamicAuthorizationPolicy{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      name,
-				Namespace: namespace,
-			},
-			Spec: v1.DynamicAuthorizationPolicySpec{
-				DynamicPolicies: []v1.DynamicPolicy{
-					dynamicPolicy,
+	tests := map[string]struct {
+		dap  *v1.DynamicAuthorizationPolicy
+		pods []*corev1.Pod
+	}{
+		"With multiple pods with same label selector": {
+			&v1.DynamicAuthorizationPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "dap",
+					Namespace: namespace,
+				},
+				Spec: v1.DynamicAuthorizationPolicySpec{
+					DynamicPolicies: []v1.DynamicPolicy{
+						dynamicPolicy,
+					},
 				},
 			},
-		}
-
-		pod := &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      name,
-				Namespace: namespace,
-				Labels:    labelSel,
+			[]*corev1.Pod{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pod-a",
+						Namespace: namespace,
+						Labels:    labelSel,
+					},
+					Spec: corev1.PodSpec{
+						Containers:         []corev1.Container{{Image: "image", Name: "container"}},
+						ServiceAccountName: "service-account",
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pod-b",
+						Namespace: namespace,
+						Labels:    labelSel,
+					},
+					Spec: corev1.PodSpec{
+						Containers:         []corev1.Container{{Image: "image", Name: "container"}},
+						ServiceAccountName: "service-account-b",
+					},
+				},
 			},
-			Spec: corev1.PodSpec{
-				Containers:         []corev1.Container{{Image: "image", Name: "container"}},
-				ServiceAccountName: "service-account",
-			},
-		}
+		},
+	}
 
-		Expect(k8sClient.Create(ctx, dap)).Should(Succeed())
-		// tests if dap gets triggered from pod change
-		if !useFakeClient {
-			time.Sleep(time.Millisecond * 1000)
-		}
-		Expect(k8sClient.Create(ctx, pod)).Should(Succeed())
-
-		dapNN := types.NamespacedName{Name: name, Namespace: namespace}
-
-		if useFakeClient {
-			dapr := DynamicAuthorizationPolicyReconciler{
-				Scheme: scheme.Scheme,
-				Client: k8sClient,
-			}
-			dapr.Reconcile(ctx, ctrl.Request{NamespacedName: dapNN})
-		}
-
-		Eventually(func(g Gomega) {
-			createdDap := v1.DynamicAuthorizationPolicy{}
-			err := k8sClient.Get(ctx, dapNN, &createdDap)
-			g.Expect(err).NotTo(HaveOccurred())
-			policy := createdDap.Status.ServiceAccountPolicyMapping[dynamicPolicy.Name]
-			expectedPolicy := fmt.Sprintf("%s/ns/%s/sa/%s",
-				dynamicPolicy.TrustDomain, pod.GetNamespace(), pod.Spec.ServiceAccountName)
-			g.Expect(policy).To(HaveKey(expectedPolicy))
-		}, timeout, interval).Should(Succeed())
-	})
+	for desc, test := range tests {
+		matchingSelectorsTest(desc, test.dap, test.pods)
+	}
 })
 
-// })
+func matchingSelectorsTest(desc string, dap *v1.DynamicAuthorizationPolicy, pods []*corev1.Pod) {
+	Describe(desc, func() {
+		It("Pod exists with mathching selectors, policies are added to DAP", func() {
+			ctx := context.Background()
 
-// var _ = Describe("DynamicAuthorizationPolicy controller", func() {
-// 	DescribeTable("When Pod is exists mathching selectors, policies are added to DAP",
-// 		func(waitBetweenCreate time.Duration, triggerReconcile bool) {
-// 			ctx := context.Background()
-//
-// 			name, namespace := "name", "default"
-// 			labelSel := map[string]string{"labelKey": "labelVal"}
-// 			dynamicPolicy := v1.DynamicPolicy{PodSelectors: labelSel, Name: "policy", TrustDomain: "cluster.local"}
-//
-// 			dap := &v1.DynamicAuthorizationPolicy{
-// 				ObjectMeta: metav1.ObjectMeta{
-// 					Name:      name,
-// 					Namespace: namespace,
-// 				},
-// 				Spec: v1.DynamicAuthorizationPolicySpec{
-// 					DynamicPolicies: []v1.DynamicPolicy{
-// 						dynamicPolicy,
-// 					},
-// 				},
-// 			}
-//
-// 			pod := &corev1.Pod{
-// 				ObjectMeta: metav1.ObjectMeta{
-// 					Name:      name,
-// 					Namespace: namespace,
-// 					Labels:    labelSel,
-// 				},
-// 				Spec: corev1.PodSpec{
-// 					Containers:         []corev1.Container{{Image: "image", Name: "container"}},
-// 					ServiceAccountName: "service-account",
-// 				},
-// 			}
-//
-// 			Expect(k8sClient.Create(ctx, dap)).Should(Succeed())
-// 			time.Sleep(waitBetweenCreate)
-// 			Expect(k8sClient.Create(ctx, pod)).Should(Succeed())
-//
-// 			dapNN := types.NamespacedName{Name: name, Namespace: namespace}
-//
-// 			if triggerReconcile {
-// 				dapr := DynamicAuthorizationPolicyReconciler{
-// 					Scheme: scheme.Scheme,
-// 					Client: k8sClient,
-// 				}
-// 				dapr.Reconcile(ctx, ctrl.Request{NamespacedName: dapNN})
-// 			}
-//
-// 			Eventually(func(g Gomega) {
-// 				createdDap := v1.DynamicAuthorizationPolicy{}
-// 				err := k8sClient.Get(ctx, dapNN, &createdDap)
-// 				g.Expect(err).NotTo(HaveOccurred())
-// 				policy := createdDap.Status.ServiceAccountPolicyMapping[dynamicPolicy.Name]
-// 				expectedPolicy := fmt.Sprintf("%s/ns/%s/sa/%s",
-// 					dynamicPolicy.TrustDomain, pod.GetNamespace(), pod.Spec.ServiceAccountName)
-// 				g.Expect(policy).To(HaveKey(expectedPolicy))
-// 			}, timeout, interval).Should(Succeed())
-// 		},
-// 		Entry("using a fake client", time.Duration(0), true, fakeClient),
-// 		Entry("using a real client", time.Duration(0), false, realClient),
-// 	)
-// })
+			Expect(k8sClient.Create(ctx, dap)).Should(Succeed())
+			if !useFakeClient {
+				// ensure dap gets triggered from later Pod change
+				time.Sleep(time.Millisecond * 1000)
+			}
+			for _, pod := range pods {
+				Expect(k8sClient.Create(ctx, pod)).Should(Succeed())
+			}
+
+			dapNN := client.ObjectKeyFromObject(dap)
+
+			if useFakeClient {
+				dapr := DynamicAuthorizationPolicyReconciler{
+					Scheme: scheme.Scheme,
+					Client: k8sClient,
+				}
+				dapr.Reconcile(ctx, ctrl.Request{NamespacedName: dapNN})
+			}
+
+			Eventually(func(g Gomega) { // nolint:varnamelen
+				createdDap := v1.DynamicAuthorizationPolicy{}
+				err := k8sClient.Get(ctx, dapNN, &createdDap)
+				g.Expect(err).NotTo(HaveOccurred())
+				for _, policy := range dap.Spec.DynamicPolicies {
+					name := policy.Name
+					selectors := policy.PodSelectors
+					for _, pod := range pods {
+						if hasSelectors(pod, selectors) {
+							podPolicy := fmt.Sprintf("%s/ns/%s/sa/%s",
+								policy.TrustDomain, pod.GetNamespace(), pod.Spec.ServiceAccountName)
+							policy := createdDap.Status.ServiceAccountPolicyMapping[name]
+							g.Expect(policy).To(HaveKey(podPolicy))
+						}
+					}
+				}
+			}, timeout, interval).Should(Succeed())
+		})
+	})
+}
+
+func hasSelectors(pod *corev1.Pod, selectors map[string]string) bool {
+	labels := pod.GetLabels()
+	if labels == nil {
+		return false
+	}
+	for k, v := range selectors {
+		if val, ok := labels[k]; !ok || val != v {
+			return false
+		}
+	}
+	return true
+}
